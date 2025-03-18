aff8f8d58b11de24e2ed5f62e550425a00040361
Phils1993 <phils1993@outlook.com>
2025-03-18 11:09:03 +0100

    made login and create HTML and half of entitites

---
+++ src/main/java/app/entitites/User.java
@@ -0,0 +1,59 @@
+package app.entitites;
+
+import java.security.Timestamp;
+
+public class User {
+    private int user_id;
+    private String username;
+    private String password;
+    private Timestamp timestamp;
+
+    public User(int user_id, String username, String password, Timestamp timestamp) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.timestamp = timestamp;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
